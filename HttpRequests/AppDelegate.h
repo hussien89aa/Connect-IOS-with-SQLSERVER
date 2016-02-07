6d84c15cd23c89f85e91a25168f3dfea690e6933
hussienalrubaye <hussien89aa@yahoo.com>
2016-02-07 16:33:50 -0500

    Initial Commit

---
+++ HttpRequests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpRequests
+//
+//  Created by hussien alrubaye on 2/7/16.
+//  Copyright © 2016 hussien alrubaye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpRequests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpRequests
+//
+//  Created by hussien alrubaye on 2/7/16.
+//  Copyright © 2016 hussien alrubaye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
